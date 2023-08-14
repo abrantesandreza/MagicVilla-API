414586ada7840d8cc76d1f111c7eeb10a92dc615
Andreza Abrantes <andrezaabrantes@gmail.com>
2023-08-14 19:32:42 -0300

    UpdateVilla done

--- MagicVilla_VillaAPI/Data/VillaStore.cs
+++ MagicVilla_VillaAPI/Data/VillaStore.cs
@@ -6,11 +6,11 @@ namespace MagicVilla_VillaAPI.Data
     {
         public static List<VillaDTO> villaList = new List<VillaDTO>
             {
-                new VillaDTO{ Id = 1, Name = "Pool View" },
-                new VillaDTO{ Id = 2, Name = "Beach View" },
-                new VillaDTO{ Id = 3, Name = "Pier View" },
-                new VillaDTO{ Id = 4, Name = "Ocean View" },
-                new VillaDTO{ Id = 5, Name = "Garden Window" }
+                new VillaDTO{ Id = 1, Name = "Pool View", Sqft = 100, Occupancy = 2 },
+                new VillaDTO{ Id = 2, Name = "Beach View", Sqft = 300, Occupancy = 3 },
+                new VillaDTO{ Id = 3, Name = "Pier View", Sqft = 250, Occupancy = 3 },
+                new VillaDTO{ Id = 4, Name = "Ocean View", Sqft = 420, Occupancy = 5 },
+                new VillaDTO{ Id = 5, Name = "Garden Window", Sqft = 400, Occupancy = 5 }
             };
     }
 }
